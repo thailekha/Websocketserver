790815b4c33588c23fc89e4f7d9f8043fded45f1
HP <HP@yahoo>
2016-02-05 04:26:14 +0700

    Make it master !

---
+++ app/controllers/Application.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+	//Dummy method to start the app
+	public static void start() {
+		
+	}
+	
+    public static void index() {
+        render();
+    }
+}
---
+++ app/controllers/Mywebsocket.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import static play.libs.F.Matcher.*;
+import static play.mvc.Http.WebSocketEvent.*;
+
+import play.Logger;
+import play.mvc.Http.WebSocketClose;
+import play.mvc.Http.WebSocketEvent;
+import play.mvc.WebSocketController;
+
+public class Mywebsocket extends WebSocketController {
+	 
+    public static void hello(String name) {
+    	Logger.info("Socket method invoked");
+        outbound.send("Hello! " + name);
+        
+        while(inbound.isOpen()) {
+            WebSocketEvent e = await(inbound.nextEvent());
+            
+            for(String quit: TextFrame.and(Equals("quit")).match(e)) {
+                outbound.send("Bye!");
+                disconnect();
+            }
+    
+            for(String message: TextFrame.match(e)) {
+            	Logger.info("Received msg");
+                outbound.send("Echo: %s", message);
+            }
+            
+            for(WebSocketClose closed: SocketClosed.match(e)) {
+                Logger.info("Socket closed!");
+            }
+       }
+        Logger.info("Socket closed!");
+    }
+}
